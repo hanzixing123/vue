a02d83a0d9937ab848e98c776b6dbf764014c96f
han <2397075084@qq.com>
2021-03-04 10:00:36 +0800

    123

--- lishang/config/index.js
+++ lishang/config/index.js
@@ -15,7 +15,7 @@ module.exports = {
         changeOrigin: true,                            // 允许跨域
         pathRewrite: { '^/api': '' }            // 接口名重写
       }
-    },
+    }, 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 2121, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
